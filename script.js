d1a3f736d4bb8502e1c6019cd68b9e0ee8a84292
Brian Verduzco <brian@Brians-MacBook-Pro.local>
2016-06-23 12:31:58 -0500

    Added index and script files

---
+++ script.js
@@ -0,0 +1,8 @@
+var avengersMovie = {
+        name : "Avengers",
+        runtime : "143 mins",
+        releaseYear : 2012,
+        director : "Joss Whedon"
+};
+
+console.log("The parts of my object are:\nname: " + avengersMovie.name + "\nruntime: " + avengersMovie.runtime + "\nreleaseYear: " + avengersMovie.releaseYear + "\ndirector: " + avengersMovie.director);
